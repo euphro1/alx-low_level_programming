5ec9c2d3e36a5c95c2a56ce30b5af0be51b2d971
euphro1 <choraichiessaadia@gmail.com>
2024-01-12 06:30:03 -0800

    First task

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,4 +1,23 @@
-#include <stdio.h>
+#include <stdio.h#include "main.h"
+
+/**
+  * _puts_recursion - main function
+  *
+  * @s: function parameter
+  *
+  * Return: Always 0.
+  */
+void _puts_recursion(char *s)
+{
+	if (*s)
+	{
+	_putchar(*s);
+	_puts_recursion(s + 1);
+	}
+	else
+	_putchar('\n');
+}
+>
 void _puts_recursion(char *s)
 {
 	if (*s == \0)
